2c57ef1f55f3234eea8ef3ecf66e8edfecfca131
eternalmarkfarm <87720623+eternalmarkfarm@users.noreply.github.com>
2026-01-10 22:46:36 +0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,29 @@
 /** @type {import('next').NextConfig} */
+
+// Проверяем: если сборка идет на GitHub Actions, то true. Иначе (на Vercel/локально) - false.
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+// Задаем имя папки только если мы на GitHub
+let assetPrefix = '';
+let basePath = '';
+
+if (isGithubActions) {
+  const repo = '/v0-telegram-mini-app-design'; // Ваше название репозитория
+  assetPrefix = repo;
+  basePath = repo;
+}
+
 const nextConfig = {
   output: 'export',
   
-  // ВАЖНО: Указываем название вашего репозитория со слэшем в начале
-  basePath: '/v0-telegram-mini-app-design',
+  // Применяем настройки динамически
+  assetPrefix: assetPrefix,
+  basePath: basePath,
   
   images: {
     unoptimized: true,
   },
   
-  // Эти настройки нужны, чтобы v0 не ругался при сборке
   eslint: {
     ignoreDuringBuilds: true,
   },
