67d2c14b8f5cabda0c0bc1e0da76a3abee4ee062
eternalmarkfarm <87720623+eternalmarkfarm@users.noreply.github.com>
2026-01-10 23:37:02 +0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,35 @@
 /** @type {import('next').NextConfig} */
+
+// Проверяем: если сборка идет на GitHub Actions, то true. Иначе (на Vercel/локально) - false.
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+// Задаем имя папки только если мы на GitHub
+let assetPrefix = '';
+let basePath = '';
+
+if (isGithubActions) {
+  const repo = '/v0-telegram-mini-app-design'; // Ваше название репозитория
+  assetPrefix = repo;
+  basePath = repo;
+}
+
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  output: 'export',
+  
+  // Применяем настройки динамически
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+  
   images: {
     unoptimized: true,
   },
- 
-}
+  
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+};
 
-export default nextConfig
+export default nextConfig;
