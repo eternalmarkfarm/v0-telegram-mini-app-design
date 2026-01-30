f6a1185f31d52d027c38afa5ebcb690fa57efa23
eternalmarkfarm <nksepte@gmail.com>
2026-01-30 15:37:33 +0500

    add

--- app/prom/components/BottomNavigation.tsx
+++ app/prom/components/BottomNavigation.tsx
@@ -3,13 +3,13 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useState } from "react";
-const iconSuperdropInactive = "/prom/medal_black.png";
+const iconSuperdropInactive = "/prom/medal_black.svg";
 const iconSuperdropActive = "/prom/medal_color.png";
 const diceIcon = "/prom/dice_bw.png";
 const diceIconActive = "/prom/dice.png";
 const homeIconMask = "/prom/home_b.svg";
-const streamerIconMask = "/prom/live_black.png";
-const forStreamersInactive = "/prom/user-setting_new.png";
+const streamerIconMask = "/prom/live_black.svg";
+const forStreamersInactive = "/prom/user-setting_new.svg";
 const forStreamersActive = "/prom/user-setting_new.png";
 
 
